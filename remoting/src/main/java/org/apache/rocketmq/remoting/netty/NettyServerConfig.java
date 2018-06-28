b9ce18a0fc1cee778a6ea63f58ba629c6f3f4d4a
Rudy <frankersun@qq.com>
2018-06-28 12:52:51 +0800

    master

---
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyServerConfig.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.remoting.netty;
+
+public class NettyServerConfig implements Cloneable {
+    private int listenPort = 8888;//默认监听端口
+    private int serverWorkerThreads = 8;//Netty服务工作线程数量
+    private int serverCallbackExecutorThreads = 0;//Netty服务异步回调线程池线程数量
+    private int serverSelectorThreads = 3;//Netty Selector线程数量
+    private int serverOnewaySemaphoreValue = 256;//控制单向的信号量
+    private int serverAsyncSemaphoreValue = 64;//控制异步信号量
+    private int serverChannelMaxIdleTimeSeconds = 120;//服务空闲心跳检测时间间隔单位秒
+
+    private int serverSocketSndBufSize = NettySystemConfig.socketSndbufSize;//Netty发送缓冲区大小
+    private int serverSocketRcvBufSize = NettySystemConfig.socketRcvbufSize;//Netty接受缓冲区大小
+    private boolean serverPooledByteBufAllocatorEnable = true;//是否使用Netty内存池
+
+    /**
+     * make make install
+     *
+     *
+     * ../glibc-2.10.1/configure \ --prefix=/usr \ --with-headers=/usr/include \
+     * --host=x86_64-linux-gnu \ --build=x86_64-pc-linux-gnu \ --without-gd
+     */
+    private boolean useEpollNativeSelector = false;
+
+    public int getListenPort() {
+        return listenPort;
+    }
+
+    public void setListenPort(int listenPort) {
+        this.listenPort = listenPort;
+    }
+
+    public int getServerWorkerThreads() {
+        return serverWorkerThreads;
+    }
+
+    public void setServerWorkerThreads(int serverWorkerThreads) {
+        this.serverWorkerThreads = serverWorkerThreads;
+    }
+
+    public int getServerSelectorThreads() {
+        return serverSelectorThreads;
+    }
+
+    public void setServerSelectorThreads(int serverSelectorThreads) {
+        this.serverSelectorThreads = serverSelectorThreads;
+    }
+
+    public int getServerOnewaySemaphoreValue() {
+        return serverOnewaySemaphoreValue;
+    }
+
+    public void setServerOnewaySemaphoreValue(int serverOnewaySemaphoreValue) {
+        this.serverOnewaySemaphoreValue = serverOnewaySemaphoreValue;
+    }
+
+    public int getServerCallbackExecutorThreads() {
+        return serverCallbackExecutorThreads;
+    }
+
+    public void setServerCallbackExecutorThreads(int serverCallbackExecutorThreads) {
+        this.serverCallbackExecutorThreads = serverCallbackExecutorThreads;
+    }
+
+    public int getServerAsyncSemaphoreValue() {
+        return serverAsyncSemaphoreValue;
+    }
+
+    public void setServerAsyncSemaphoreValue(int serverAsyncSemaphoreValue) {
+        this.serverAsyncSemaphoreValue = serverAsyncSemaphoreValue;
+    }
+
+    public int getServerChannelMaxIdleTimeSeconds() {
+        return serverChannelMaxIdleTimeSeconds;
+    }
+
+    public void setServerChannelMaxIdleTimeSeconds(int serverChannelMaxIdleTimeSeconds) {
+        this.serverChannelMaxIdleTimeSeconds = serverChannelMaxIdleTimeSeconds;
+    }
+
+    public int getServerSocketSndBufSize() {
+        return serverSocketSndBufSize;
+    }
+
+    public void setServerSocketSndBufSize(int serverSocketSndBufSize) {
+        this.serverSocketSndBufSize = serverSocketSndBufSize;
+    }
+
+    public int getServerSocketRcvBufSize() {
+        return serverSocketRcvBufSize;
+    }
+
+    public void setServerSocketRcvBufSize(int serverSocketRcvBufSize) {
+        this.serverSocketRcvBufSize = serverSocketRcvBufSize;
+    }
+
+    public boolean isServerPooledByteBufAllocatorEnable() {
+        return serverPooledByteBufAllocatorEnable;
+    }
+
+    public void setServerPooledByteBufAllocatorEnable(boolean serverPooledByteBufAllocatorEnable) {
+        this.serverPooledByteBufAllocatorEnable = serverPooledByteBufAllocatorEnable;
+    }
+
+    public boolean isUseEpollNativeSelector() {
+        return useEpollNativeSelector;
+    }
+
+    public void setUseEpollNativeSelector(boolean useEpollNativeSelector) {
+        this.useEpollNativeSelector = useEpollNativeSelector;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return (NettyServerConfig) super.clone();
+    }
+}
