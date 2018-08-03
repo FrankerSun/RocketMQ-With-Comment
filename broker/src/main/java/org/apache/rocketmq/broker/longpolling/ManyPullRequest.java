5b19d4d78ccc63effce9fd0fa8e8373bb6016ac2
Rudy <frankersun@qq.com>
2018-08-03 20:59:24 +0800

    pull request

--- broker/src/main/java/org/apache/rocketmq/broker/longpolling/ManyPullRequest.java
+++ broker/src/main/java/org/apache/rocketmq/broker/longpolling/ManyPullRequest.java
@@ -19,6 +19,9 @@ package org.apache.rocketmq.broker.longpolling;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 多个pull request请求
+ */
 public class ManyPullRequest {
     private final ArrayList<PullRequest> pullRequestList = new ArrayList<>();
 
